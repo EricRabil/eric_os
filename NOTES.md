0192b4507606f9e90444548fc8e2eaf9a0fbfd47
Eric Rabil <ericjrabil@gmail.com>
2020-04-18 21:24:39 -0400

    Convert project to a freestanding binary

---
+++ NOTES.md
@@ -0,0 +1,7 @@
+# Rust Notes
+
+## Using Cargo
+
+**New Project**
+> `cargo init --bin --edition 2018`
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+// disable stdlib to make this a freestanding library
+#![no_std]
+// disable standard entry point in rust program, i am god.
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[panic_handler]
+// -> ! implies that a function never returns.
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+// again, ! implies this function never returns. that makes sense because this is a KERNEL, bitch.
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
